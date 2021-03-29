e6c634e75682985be019a1d2ebab2111f53f8558
michal.mrosko <michal.mrosko@rohlik.cz>
2021-03-29 19:45:50 +0200

    Cleanup

--- src/main/java/com/mroskino/play/websocketserver/handler/TestWebsocketHandler.java
+++ src/main/java/com/mroskino/play/websocketserver/handler/TestWebsocketHandler.java
@@ -2,9 +2,7 @@ package com.mroskino.play.websocketserver.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.socket.WebSocketHandler;
-import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -12,12 +10,10 @@ public class TestWebsocketHandler implements WebSocketHandler {
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
-        Flux<WebSocketMessage> output = session.receive()
+        return session.send(session.receive()
                 .doOnNext(message -> {
                     log.info("Received message: {}", message.getPayloadAsText());
                 })
-                .map(value -> session.textMessage("Echo " + value.getPayloadAsText()));
-
-        return session.send(output);
+                .map(value -> session.textMessage("Echo " + value.getPayloadAsText())));
     }
 }
